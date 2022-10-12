b4cab5330e88a799c5b9ae475b39ec78134e4370
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2022-10-12 13:35:18 +0200

    mise en place du boutton switch

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -6,6 +6,15 @@ const Header = () =>{
 
     return(
         <div id="header">
+
+            <div id="header__container__switch">
+                <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+                <p>Alternative style</p>
+            </div>
+
             <h1 id="header__h1">
                 {titre}
             </h1>
